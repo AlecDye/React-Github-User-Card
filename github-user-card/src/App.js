37d060e4245b237db82f3b931a97318c1cc1e9bc
Alec Dye <alecdye@gmail.com>
2020-02-11 15:13:04 -0800

    building out components

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -9,29 +9,48 @@ class App extends React.Component {
   constructor() {
     console.log("Constructor firing")
     super();
+    this.state = {
+      userList: [],
+      followerList: []
+    }
   }
 
   componentDidMount() {
+    // fetching API
     console.log("CDM firing")
     axios
       .get("https://api.github.com/users/alecdye")
       .then(res => {
-        console.log(res.data)
         // res.data
         this.setState({
-          users: res.data
+          userList: res.data
+        })
+        // console.log(res.data)
+      })
+      .catch(err => console.log("User error", err))
+
+    axios
+      .get("https://api.github.com/users/alecdye/followers")
+      .then(res => {
+        this.setState({
+          followerList: res.data
         })
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log("Follower error", err))
   }
 
   render() {
+    // rendering components
     console.log("Rendering...")
     return (
       <div className="App">
         <header className="App-header">
           GitHub Users!
-      </header>
+          {/* UserCard component this.state.userList
+              Follower Card component this.state.followerList */}
+        </header>
+        <div className="users">
+        </div>
       </div>
     );
   }
