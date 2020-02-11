1f0b24932316d82fa24a43e951642bab4b0348b0
Alec Dye <alecdye@gmail.com>
2020-02-11 13:12:18 -0800

    fetching data and assigning to state

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -5,8 +5,28 @@ import './App.css';
 //! Use Class Components only! NO useEffect
 
 class App extends React.Component {
+  // initializing state
+  constructor() {
+    console.log("Constructor firing")
+    super();
+  }
+
+  componentDidMount() {
+    console.log("CDM firing")
+    axios
+      .get("https://api.github.com/users/alecdye")
+      .then(res => {
+        console.log(res.data)
+        // res.data
+        this.setState({
+          users: res.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
 
   render() {
+    console.log("Rendering...")
     return (
       <div className="App">
         <header className="App-header">
